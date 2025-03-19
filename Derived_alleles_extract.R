e77beba7818484dd8da23f8ef17ecefe68625217
BeckySTaylor <85310401+BeckySTaylor@users.noreply.github.com>
2025-03-19 15:02:04 -0400

    Update Derived_alleles_extract.R
    Make it run with a control file for input settings

--- Derived_alleles_extract.R
+++ Derived_alleles_extract.R
@@ -1,10 +1,32 @@
+# Control file format - a .txt file where each line is the following:
+# VCF file path
+# Rates file path
+# Minimum GERP score
+# Output file name (without file extension)
+# Number of threads/cores
+
+# Assign command-line arguments to a variable
+args = commandArgs(trailingOnly=TRUE)
+
+# Check if no arguments have been provided. If not, print message
+#   that control file needs to be provided
+if (length(args)==0) {
+  stop("No control file provided", call.=FALSE)
+}
+
 library(vcfR)
 library(tidyverse)
 library(data.table)
 library(magrittr)
 library(parallel)
 
-vcf <- read.vcfR("./Input_VCF_Scaffold1.vcf.gz")
+# Read in the name of the control file (given as argument 1)
+# Each line of the 'ctrl' data frame is an element from the
+#  control file.
+ctrl = read.delim(args[1],header=F)
+ctrl[,1] = as.character(ctrl[,1])
+
+vcf <- read.vcfR(ctrl[1,1])
 vcf_field_names(vcf, tag = "FORMAT")
 Z <- vcfR2tidy(vcf, format_fields = c("GT", "DP"))
 gt2 = Z$gt
@@ -14,11 +36,16 @@ gt2 %<>%
   mutate("ref" = str_remove(gt_GT_alleles, "(/|\\|).$"),
          "tar" = str_remove(gt_GT_alleles, "^.(/|\\|)"));
 
-sca = fread("./Scaffold15.fasta.rates", nThread = 8) %>%
+sca = fread(ctrl[2,1], nThread = as.numeric(ctrl[5,1])) %>%
   mutate(Pos = Pos + 1) %>%
-  filter(V2 > 0)
+  filter(V2 != -1) %>%
+  filter(V2 > as.numeric(ctrl[3,1]))
 
 sams = gt2$Indiv %>% unique();
+
+sp1 = names(sca)[4]
+sp2 = names(sca)[5]
+sp3 = names(sca)[6]
 
 out = parallel::mclapply(seq_len(length(sams)),
                          FUN = function(i){
@@ -32,15 +59,15 @@ out = parallel::mclapply(seq_len(length(sams)),
                              filter(Pos %in% tmp$POS) -> tmp2;
 
                            left_join(tmp2, tmp, by = c("Pos" = "POS")) %>%
-                             filter((tar !=  Odocoileus_virginianus &
-                                      tar != Alces_alces &
-                                      tar != Cervus_elaphus) |
-                                      (ref !=  Odocoileus_virginianus &
-                                      ref != Alces_alces &
-                                      ref != Cervus_elaphus)) %>%
+                             filter((tar !=  !!as.symbol(sp1) &
+                                       tar != !!as.symbol(sp2) &
+                                       tar != !!as.symbol(sp3)) |
+                                      (ref != !!as.symbol(sp1) &
+                                         ref != !!as.symbol(sp2) &
+                                         ref != !!as.symbol(sp3))) %>%
                              mutate("sam" = sams[i]) -> out;
 
-                           fwrite(out, paste0(i, "_Scaffold15_over2.txt"));#write to drive
+                           fwrite(out, paste0(i, "_", ctrl[4,1],".txt"));#write to drive
 
                            return(out);
-                         }, mc.preschedule = F, mc.cores = 8)
+                         }, mc.preschedule = F, mc.cores = as.numeric(ctrl[5,1]))
